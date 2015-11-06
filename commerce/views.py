cc8d83039c221457d65db5b944a3ddaf36e1771e
Clara <clara.081094@gmail.com>
2015-11-06 17:08:13 -0500

    Login funcionando

--- commerce/views.py
+++ commerce/views.py
@@ -9,8 +9,33 @@ from django.http import HttpResponseRedirect
 from django.contrib import messages
 #import RPi.GPIO as GPIO
 
+@csrf_exempt
+@login_required(login_url='/login/')
 def index(request):
-#    return render_to_response('commerce/index.html')
+    return render_to_response('commerce/index.html')
 
-#def login(request):
-	return render_to_response('commerce/login.html')
+@csrf_exempt
+def login_user(request):
+
+    if request.user.is_authenticated():
+        return HttpResponseRedirect('/commerce/')
+
+    logout(request)
+    state = "Ingrese su usuario y password"
+    username = password = ''
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                request.session.set_expiry(60)
+                login(request, user)
+                return HttpResponseRedirect('/commerce/')
+            else:
+                state = "Su cuenta no existe, contacte con el administrador"
+        else:
+            state = "Su username y/o password son incorrectos."
+
+    return render_to_response('commerce/login.html',{'state':state, 'username': username})
